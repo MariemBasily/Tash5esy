4693813b9f1a7bc56aa8af06134a325db082e0aa
MariemBasily <mariembasily01@gmail.com>
2024-09-20 21:21:46 +0200

    current update & solved all conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:tash5esy_app/models/splash_model.dart';
 import 'package:tash5esy_app/views/login/login_screen.dart';
 import 'package:tash5esy_app/views/splash/splash_screen.dart';
 import 'package:tash5esy_app/views/get_started/get_started_screen.dart';
-import 'package:tash5esy_app/views/signup/signup_screen.dart'; 
+//import 'package:tash5esy_app/views/signup/signup_screen.dart'; 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();

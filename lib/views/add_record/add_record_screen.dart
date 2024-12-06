1464bc11d69c1711cb949fdd96d6713a56a5e9ad
Salma Adel <salmaadel2206@gmail.com>
2024-12-06 11:06:11 +0200

    Your commit message describing the changes

--- lib/views/add_record/add_record_screen.dart
+++ lib/views/add_record/add_record_screen.dart
@@ -3,7 +3,6 @@ import 'package:tash5esy_app/core/components/custom_button.dart';
 import 'package:tash5esy_app/core/style/colors.dart';
 import 'package:tash5esy_app/views/add_record/widgets/widget.dart';
 import 'package:tash5esy_app/views/general_information/general_information_screen.dart';
-import 'package:tash5esy_app/views/medical_record/medical_history_screen.dart';
 
 class AddRecordScreen extends StatelessWidget {
   @override
